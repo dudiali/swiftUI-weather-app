d0acd00e03bf892ec389614a0cbcd280bee7aafb
dudialimurtado <dudimurtado19@gmail.com>
2025-05-26 09:12:08 +0700

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Macbook on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoTests/TodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTests.swift
+//  TodoTests
+//
+//  Created by Macbook on 26/05/25.
+//
+
+import Testing
+@testable import Todo
+
+struct TodoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
